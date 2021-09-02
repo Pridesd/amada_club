46861d600e93a02d08cf6df6fc97fa21c92398c1
Pridesd <whwotjr98@naver.com>
2021-09-02 17:20:11 +0900

    산악기상 API 적용(Hiking.html 작동 오류 수정 요망)

--- amadaclub/hiking/views.py
+++ amadaclub/hiking/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.http import request
+from .crawling import *
 
 def hiking(request):
-    return render(request, 'hiking.html')
+    weather = mountain_weather(1)
+    temp = {}
+    wind = {}
+    humanity = {}
+    rain = {}
+    for number in range(len(weather)):
+        temp[weather[number]['obsname']] = weather[number]['tm2m'] #기온
+        humanity[weather[number]['obsname']] = weather[number]['hm2m'] #습도
+        wind[weather[number]['obsname']] = weather[number]['ws2m'] #풍속
+        rain[weather[number]['obsname']] = weather[number]['rn'] #강수량
+    
+    weather_result = {'temp':temp, 'wind':wind, 'humanity':humanity, 'rain':rain}
+    return render(request, 'hiking.html', weather_result)
 
 # Create your views here.
