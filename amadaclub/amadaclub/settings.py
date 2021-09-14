dd8f96ff558f4171de4aad467749b15ce2ac3917
Pridesd <whwotjr98@naver.com>
2021-09-14 20:21:01 +0900

    제발 좀 되라

--- amadaclub/amadaclub/settings.py
+++ amadaclub/amadaclub/settings.py
@@ -33,9 +33,9 @@ environ.Env.read_env(
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['141.164.55.175:8000', 'localhost', '141.164.55.175']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
